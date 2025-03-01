51eafda6e9f458deaa21549c22be454753353731
Yevgeniy <ulchenkov2001@gmail.com>
2025-03-01 15:40:23 +0100

    [Test] build test

--- src/test/java/cz/cvut/moviemate/userservice/UserServiceApplicationTests.java
+++ src/test/java/cz/cvut/moviemate/userservice/UserServiceApplicationTests.java
@@ -2,6 +2,8 @@ package cz.cvut.moviemate.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class UserServiceApplicationTests {
