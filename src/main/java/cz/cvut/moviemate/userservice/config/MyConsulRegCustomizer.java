ff4286ad4446f9765100c60d2d7a3fb55b74afd1
Yevgeniy <94138754+ulcheyev@users.noreply.github.com>
2025-09-09 08:04:01 +0200

    Dev/release test (#6)
    * [Test] build test
    
    * [Test] skip tests added
    
    * [Test] skip tests added
    
    * [Test] fix envs
    
    * [Test] fix skip tests
    
    * [Test] added explicitly skip tests
    
    * [Test] added explicitly skip tests
    
    * [Test] added explicitly skip tests
    
    * [Test] added explicitly skip tests
    
    * [Test] added explicitly skip tests
    
    * [Upd] added logic for starting all stack
    
    * [Upd] added discovery server client and vars
    
    * [Fix] reformat code
    
    * [Fix] added dymanic server context path
    
    * [Upd] added headers in token validation
    
    * [Test] traefik conf changed
    
    * [fix] fixed consul tags for context path
    
    * [fix] fixed consul tags -- added quotes
    
    * [upd] name of app. Traefik config changed based on new name.
    
    * [upd] name of app. Traefik config.
    
    * [fix] changed traefik config to file.
    
    * [fix] replaced dapr port with standard private port
    
    * [upd] otl metrics scrapper integration
    
    * [fix] dash to complete the build command
    
    * [test] testing deploy with otel in config file
    
    * [test] testing deploy with otel in config file
    
    * [test] testing deploy with otel in config file
    
    * [Upd] changed standard port. Rewrote readme
    
    * [Upd] Rewrote readme -- use all envs to describe configuration
    
    * [Upd] add missing envs.
    
    * [Upd] .env.example and readme.
    
    * [Upd] validate token now has Get Mapping
    
    * [Upd] added regexp for public auth
    
    * [Upd] added regexp for public auth
    
    * [Upd] added regexp for public auth
    
    * [Upd] added regexp for public auth
    
    * [Upd] added header in validate token response
    
    * [Upd] Added dapr kafka pub/sub
    
    * [Upd] added missin final for dapr event service
    
    * [Upd] added dependencies for dapr
    
    * [Upd] added dependencies for dapr
    
    * [Upd] added dependencies for dapr
    
    * [Upd] added dependencies for dapr
    
    * [Upd] changed Instant to Date due to support
    
    * [Upd] remove api responses to test swagger in cluster
    
    * [Upd] refactored search users to test in swagger
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] added packages to scan property
    
    * [Upd] configured cors
    
    * [Upd] configured cors
    
    * [Fix] user history creation date added
    
    * [Fix] added updated at after registration
    
    * [Upd] added OpenAPI path in README.md
    
    * [Fix] fixed register event publication
    
    * [Upd] add log for cors allowed origins
    
    * [Fix] added method OPTIONS for cors
    
    * [Fix] path with excluded auth fixed.
    
    * [Fix] path for excluded auth.
    
    * [Fix] ags as env vars
    
    * [Fix] added customizer for consul tas
    
    * [Fix] consult tags customizer aded
    
    ---------
    
    Co-authored-by: ulcheyev <ulcheyev@fel.cvut.cz>

---
+++ src/main/java/cz/cvut/moviemate/userservice/config/MyConsulRegCustomizer.java
@@ -0,0 +1,39 @@
+package cz.cvut.moviemate.userservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.consul.serviceregistry.ConsulRegistration;
+import org.springframework.cloud.consul.serviceregistry.ConsulRegistrationCustomizer;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class MyConsulRegCustomizer implements ConsulRegistrationCustomizer {
+
+
+    @Value("${spring.cloud.consul.discovery.tags}")
+    private String rawTags;
+
+    @Override
+    public void customize(ConsulRegistration registration) {
+        List<String> tags = registration.getService().getTags();
+        if (tags == null) {
+            tags = new ArrayList<>();
+        }
+
+        // Parse custom tags from MOVIE_MATE_USER_SERVICE_LB_TAGS, splitting by newline
+        if (rawTags != null && !rawTags.isBlank()) {
+            String[] customTags = rawTags.split("\\n");
+            for (String tag : customTags) {
+                if (!tag.isBlank()) {
+                    tags.add(tag.trim());
+                }
+            }
+        }
+
+        // Set back to registration in case the tags list was replaced
+        registration.getService().setTags(tags);
+    }
+}
